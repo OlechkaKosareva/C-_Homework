ad60ccfa2a1d335eee98146186fe3873a10bd84e
Ольга <olga_kosareva22@mail.ru>
2022-08-07 20:08:28 +0300

    решение 3й задачи

---
+++ Sem2_Example003/Program.cs
@@ -0,0 +1,16 @@
+﻿//Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+//и проверяет, является ли этот день выходным.
+int n = new Random().Next(1,7);
+Console.WriteLine(n);
+if (n==1)
+Console.WriteLine("нет");
+if (n==2)
+Console.WriteLine("нет");
+if (n==3)
+Console.WriteLine("нет");
+if (n==4)
+Console.WriteLine("нет");
+if (n==5)
+Console.WriteLine("нет");
+if (n==6)
+Console.WriteLine("да");
