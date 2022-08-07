be03287e32ce17dcaf6a8635522921b4e2ac92bb
Ольга <olga_kosareva22@mail.ru>
2022-08-07 19:02:21 +0300

    добавлено решение 1й задачи

--- Sem2_Example001/Program.cs
+++ Sem2_Example001/Program.cs
@@ -1,2 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Напишите программу, которая принимает на вход трёхзначное число и 
+//на выходе показывает вторую цифру этого числа.
+int a = new Random().Next(100,999);
+Console.WriteLine(a);
+int value1 = a%100;
+int value2 = value1/10;
+Console.WriteLine(value2);
+
+
