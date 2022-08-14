c44e2623279180411c8cd1510e743e571e2fd1fa
Ольга <olga_kosareva22@mail.ru>
2022-08-14 11:20:13 +0300

    решение 3й задачи дз 3 семинар

---
+++ Sem3_Example003/Program.cs
@@ -0,0 +1,13 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+int n = new Random().Next(1,10);
+Console.WriteLine(n);
+int i = 0;
+int cube = 0;
+while (i<=n) 
+{
+    cube = i*i*i;
+    i++;
+Console.WriteLine("{0}\t{1}",i,cube);
+}
+
+
