60bd1b59a3d9840f5d8be5f678edb407af149627
Ольга <olga_kosareva22@mail.ru>
2022-08-14 13:17:07 +0300

    добавлено решение 1й задачи дз 4й семинар

---
+++ Sem4_Example001/Program.cs
@@ -0,0 +1,7 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+//и возводит число A в натуральную степень B.
+int a = new Random().Next(1,10);
+int b = new Random().Next(1,10);
+Console.WriteLine($"{a} {b}");
+double degree = Math.Pow(a,b);
+Console.WriteLine(degree);
