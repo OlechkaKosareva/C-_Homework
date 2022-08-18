1fec1c7f7675d237c56ecf12a51e7c09caf60240
Ольга <olga_kosareva22@mail.ru>
2022-08-18 22:23:54 +0300

    добавлено решение 3й задачи дз 4й семинар

---
+++ Sem4_Example003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит 
+//их на экран.
+//int[] array = {1,2,3,4,5,6,7,8};
+
+int size = 8;
+int[] func1 (int size){ 
+
+  int[] array = new int[size];    
+  Random value = new Random();
+
+    for (int i = 0; i < size; i++){
+    array[i]=value.Next(0,10);
+    }
+    return array;
+}
+// функция  вывода массива на печать 
+void func2 (int[] array){
+  for (int i=0; i < array.Length; i++)
+{
+Console.Write(array[i] + " ");
+}
+}
+  func2(func1(size));
+  
