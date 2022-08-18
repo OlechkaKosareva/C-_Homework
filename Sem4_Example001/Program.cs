f43f6eed88129b9db9d45e7412fb5c587fd11b1c
Ольга <olga_kosareva22@mail.ru>
2022-08-18 21:10:43 +0300

    решение задачи через цикл и функцию

--- Sem4_Example001/Program.cs
+++ Sem4_Example001/Program.cs
@@ -1,7 +1,21 @@
 ﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
 //и возводит число A в натуральную степень B.
-int a = new Random().Next(1,10);
-int b = new Random().Next(1,10);
-Console.WriteLine($"{a} {b}");
-double degree = Math.Pow(a,b);
-Console.WriteLine(degree);
+// int a = new Random().Next(1,10);    по ошибке начала решать задачу в лоб (без цикла)
+// int b = new Random().Next(1,10);
+// Console.WriteLine($"{a} {b}");
+// double degree = Math.Pow(a,b);
+// Console.WriteLine(degree);
+
+int funcExp(int A, int B)
+{
+int value = 1; //переменная, в которую будет записываться произведение а в степени b
+for(int i=1; i <= B; i++)
+{
+        value = value * A;
+}
+        return value;
+}
+int A = Convert.ToInt32(Console.ReadLine());
+int B = Convert.ToInt32(Console.ReadLine());
+  int functExp = funcExp(A, B);
+  Console.WriteLine(functExp);
