cdeff3d2734935796455aa718d254036c13294be
Ольга <olga_kosareva22@mail.ru>
2022-08-28 13:21:35 +0300

    добавлено решение 1й задачи дз семинар 7

---
+++ Sem07_Example001/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 47. Задайте двумерный массив размером m×n, 
+//заполненный случайными вещественными числами.
+int n = new Random().Next(1, 10);
+int m = new Random().Next(1, 10);
+Random random = new Random();
+
+void PrintArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            Console.Write($"{Math.Round(matr[i, j],2)}   ");
+        }
+    Console.WriteLine();
+    }
+}
+
+void FillArray(double[,] matr)
+{
+    for (int i = 0; i < matr.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(1); j++)
+        {
+            matr[i,j] = random.NextDouble()*100; //от 0 до 100
+
+        }
+
+    }
+}
+double[,] matrix = new double[n, m];
+FillArray(matrix);
+PrintArray(matrix);
